62e7f7a11ae3792a885b4188a00bc84137920445
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-09 15:48:37 +0530

    operators basics 1) arithmetic

---
+++ src/Operators.java
@@ -0,0 +1,13 @@
+public class Operators {
+    public static void main(String[] args) {
+        // arithmatic operator
+        //add//sub//multi//div
+        int a=5;
+        int b=9;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a*b);
+        System.out.println(a/b);
+
+    }
+}
