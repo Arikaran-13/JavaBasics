2ce9d5efa2711c9e19f04bf09decca5fd205c82f
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-09 21:55:14 +0530

    operators basics arithmetic,relational,bitwise

--- src/Operators.java
+++ src/Operators.java
@@ -1,13 +1,40 @@
 public class Operators {
     public static void main(String[] args) {
-        // arithmatic operator
+        // arithmatic operator // +,-,*,/,%
+        //relational operator // > , < >=, <= , ==, !=
+        //logical
+        //bitwise
+        //increment or decrement operators
+
         //add//sub//multi//div
         int a=5;
         int b=9;
-        System.out.println(a+b);
-        System.out.println(a-b);
-        System.out.println(a*b);
-        System.out.println(a/b);
+        System.out.println(a>b);//greater
+        System.out.println(a<b);//lesser
+        System.out.println(a>=b); //a>b or a==b // a greater thn or equal to b
+        System.out.println(a<=b);// b greater thn or equal to a
+        System.out.println(a==b); // % => remainder
+        System.out.println(a!=b);// not equal
+
+        //bitwise operator
+        // binary numbers
+        int c=2; //integer
+        int d =3;
+
+        System.out.println(2 & 3);
+        System.out.println(2 | 3);
+        System.out.println(2^3);
+
+        // 35 , 25
+        // bianry num
+        // 35 & 25
+        // 35 ! 25
+
+       // 1001 , 1100  => decimal number
+//
+
+
+
 
     }
 }
