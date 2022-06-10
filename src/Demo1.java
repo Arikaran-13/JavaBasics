c69769bdfb85b08494ffa50ccc6f7edbef658976
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-10 19:01:32 +0530

    operators basics arithmetic,relational,bitwise

---
+++ src/Demo1.java
@@ -0,0 +1,13 @@
+public class Demo1 {
+
+    public static void main(String[] args) {
+        int a= 5;
+
+        if(a>2 || a%2==0){
+            System.out.println("Yes");
+        }
+        else{
+            System.out.println("No");
+        }
+    }
+}
