23599120afca98de70b446d5809f8a2dcbe910a9
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-10 20:24:01 +0530

    array intro

---
+++ src/Arraydemo.java
@@ -0,0 +1,13 @@
+public class Arraydemo {
+    public static void main(String[] args) {
+        int [] marks   = new int[5];
+        marks[0]=100;
+        marks[1]=89;
+        marks[2]= 80;// hard coding
+        marks[3]=90;
+        marks[4]=99;
+        System.out.println(marks[0]);
+
+
+    }
+}
