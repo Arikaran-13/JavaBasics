9eed551c3e64dba7682533e8c4afc80d9f250e53
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-16 19:47:59 +0530

    constructor notes txt class example

--- src/Arraydemo.java
+++ src/Arraydemo.java
@@ -1,12 +1,10 @@
+import java.util.Arrays;
+
 public class Arraydemo {
     public static void main(String[] args) {
-        int [] marks   = new int[5];
-        marks[0]=100;
-        marks[1]=89;
-        marks[2]= 80;// hard coding
-        marks[3]=90;
-        marks[4]=99;
-        System.out.println(marks[0]);
+       String [] arr ={"a","b","c","c","d"};
+         Arrays.asList(arr).stream().distinct().forEach(System.out::println);
+        //System.out.println(ans);
 
 
     }
