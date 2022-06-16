bc4db2e329a56ab1c54a426bad1377ef0039e751
ARIKARAN S <91272168+Arikaran-13@users.noreply.github.com>
2022-06-16 19:47:30 +0530

    constructor

--- src/ConstructorDemo.java
+++ src/ConstructorDemo.java
@@ -1,9 +1,11 @@
 public class ConstructorDemo { // constructor
 
     public static void main(String[] args) {
-        School s = new School(); // constructor
-        s.setName("jvis");
+        School s = new School("jvis",12); // constructor
         System.out.println(s.getName());
+        System.out.println(s.getclasses());
+
+
     }
 }
 class School{
@@ -11,8 +13,16 @@ class School{
     private String sklname;
     private int classes;
 
+    School(String str,int cl){
+        sklname =str;
+        classes=cl;
+
+    }
+
+
     public String getName(){return sklname;}
     public void setName(String n){sklname = n;};
+    public int getclasses(){return classes;}
 
 
 }
